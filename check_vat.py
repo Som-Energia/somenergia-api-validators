5de019acaffdad74217d188a06d7cf3b0f0e1a50
César López Ramírez <cesar.lopez@somenergia.coop>
2016-06-22 11:54:41 +0200

    Check for conn & erp errors

--- check_vat.py
+++ check_vat.py
@@ -4,12 +4,21 @@ from requests import Session
 import sys
 session = Session()
 try:
-  vat = sys.argv[1]
-except:
-  print "Use: check_vat.py VAT"
-  sys.exit(1)
+    vat = sys.argv[1]
+except IndexError:
+    print "Use: check_vat.py VAT"
+    sys.exit(1)
 api = 'https://api.somenergia.coop/check/vat/'
-response = session.get(api+vat)
+try:
+    response = session.get(api+vat)
+    response.raise_for_status()
+except:
+    print "Connection error"
+    sys.exit(2)
+        
 responseJson= response.json()
+if responseJson['status'] != 'ONLINE':
+    print 'ERP is not up'
+    sys.exit(3)
 print "El DNI {} és {}".format(vat, 
     "vàlid" if responseJson['state'] else "invàlid")
