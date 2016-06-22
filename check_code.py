20499313d01bf6db29779a0f98bfec8fb0ead7c9
César López Ramírez <cesar.lopez@somenergia.coop>
2016-06-22 14:05:19 +0200

    Added CUPS validation

--- check_code.py
+++ check_code.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 from requests import Session
 API_BASE_URL='https://api.somenergia.coop'
+API_BASE_URL_TESTING='https://testing.somenergia.coop:5001'
+#API_BASE_URL=API_BASE_URL_TESTING
 def check_code(url,code):
     session = Session()
     try:
         response = session.get(API_BASE_URL+url+code)
         response.raise_for_status()
-        responseJson= response.json()
+        response = response.json()
     except:
         print "Connection error"
         raise
-    if responseJson['status'] != 'ONLINE':
+    if response['status'] != 'ONLINE':
         print 'ERP is not up'
         raise
-    return responseJson
+    return response
---
+++ check_cups.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from requests import Session
+from check_code import check_code
+import sys
+try:
+    response = check_code('/check/cups/',sys.argv[1])
+except IndexError:
+    print "Use: check_cups.py CNAE"
+    sys.exit(1)
+except:
+    sys.exit(2)
+print u"El CUPS {} és {}".format(sys.argv[1], 
+    u"invàlid, reason: {}".format(
+        response['data']['invalid_fields'][0]['error']) 
+    if not response['state'] else u"vàlid",
+    )
