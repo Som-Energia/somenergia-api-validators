b0b8566d133c5ae9872183bdd0d2a6879c6e4f07
César López Ramírez <cesar.lopez@somenergia.coop>
2016-06-22 12:59:50 +0200

    Check cnae script shows description

---
+++ check_cnae.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from requests import Session
+from check_code import check_code
+import sys
+session = Session()
+try:
+    response = check_code('/check/cnae/',sys.argv[1])
+except IndexError:
+    print "Use: check_cnae.py CNAE"
+    sys.exit(1)
+except:
+    sys.exit(2)
+print u"El CNAE {} és {}".format(sys.argv[1], 
+    u"vàlid i és per {}".format(
+        response['data']['description']) if response['state'] else u"invàlid",
+    )
