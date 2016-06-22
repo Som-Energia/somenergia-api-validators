335d1e31183e93fea8f784ea9ad1f497d6cbdfa0
César López Ramírez <cesar.lopez@somenergia.coop>
2016-06-22 15:42:53 +0200

    Added IBAN validation script

---
+++ check_iban.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from requests import Session
+from check_code import check_code
+import sys
+try:
+    response = check_code('/check/iban/',sys.argv[1])
+except IndexError:
+    print "Use: check_iban.py IBAN"
+    sys.exit(1)
+except:
+    sys.exit(2)
+print u"El IBAN {} és {}".format(sys.argv[1], 
+    u"vàlid" if response['state'] else u"invàlid",
+)
